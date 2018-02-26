77569a717b200039abb419d775ea36a1b97799ad
Alexander Kuzovlev <basistadom@gmail.com>
2018-02-26 16:54:45 +0300

    some fixes

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -11,5 +12,5 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_ADMIN', 100001);
 
 
-INSERT INTO meals (datetime, description,calories) VALUES
-  ('2015-06-01 14:00','Админ ланч' ,510);
+INSERT INTO meals (id, datetime, description,calories) VALUES
+  (2,'2015-06-01 14:00','Админ ланч' ,510);
